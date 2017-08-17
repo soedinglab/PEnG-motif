71986986b365f5a5fbc36b21cba485be2f908d7e
Christian Roth <christian.roth@mpibpc.mpg.de>
2017-08-17 14:20:42 +0200

    mnt: improved progress logging

--- src/peng.cpp
+++ src/peng.cpp
@@ -292,6 +292,13 @@ void Peng::merge_iupac_patterns(const size_t pattern_length, const float merge_b
   }
 }
 
+inline void print_status(std::string header, bool leading_newline=true) {
+  if(leading_newline) {
+    std::cout << std::endl;
+  }
+  std::cout << "[STATUS] " << header << ":" << std::endl;
+}
+
 void Peng::process(PengParameters& params, std::vector<IUPACPattern*>& best_iupac_patterns) {
 
   if(params.max_pattern_length > log(SIZE_MAX) / log(IUPAC_ALPHABET_SIZE) - 1 ||
@@ -302,27 +309,29 @@ void Peng::process(PengParameters& params, std::vector<IUPACPattern*>& best_iupa
   }
 
   for(size_t pattern_length = std::min(6, params.max_pattern_length); pattern_length <= params.max_pattern_length; pattern_length += 2) {
-    std::cout << "[STATUS] Processing kmers of length " << pattern_length << std::endl;
-
-    std::cout << "[STATUS] Finding overrepresented kmers (base patterns)" << std::endl;
+    print_status("Processing kmers of length " + std::to_string(pattern_length), false);
+    print_status("Finding overrepresented kmers (base patterns)", false);
     BasePattern* base_pattern = new BasePattern(pattern_length, strand, k, max_k, sequence_set, bg_model);
     size_t* pattern_counter = base_pattern->getPatternCounter();
 
     std::vector<size_t> filtered_base_patterns;
     base_pattern->filter_base_patterns(params.zscore_threshold, params.count_threshold, filtered_base_patterns);
 
+
+    print_status("Optimizing base patterns");
     std::cout << std::endl;
-    std::cout << "[STATUS] Optimizing base patterns" << std::endl << std::endl;
+
     std::vector<IUPACPattern*> unoptimized_iupac_patterns;
     optimize_iupac_patterns(params.opt_score_type, base_pattern, filtered_base_patterns,
     						unoptimized_iupac_patterns, params.enrich_pseudocount_factor);
     std::cout << std::endl;
-    std::cout << "[STATUS] Filtering degenerated IUPAC patterns" << std::endl;
+    print_status("Filtering degenerated IUPAC patterns");
     filter_iupac_patterns(pattern_length, unoptimized_iupac_patterns);
     for(auto pattern : unoptimized_iupac_patterns) {
       std::cout << "selected iupac pattern: " << IUPACPattern::toString(pattern->get_pattern(), pattern_length) << std::endl;
     }
 
+    print_status("Calculating PWMs");
     #pragma omp parallel for
     for(int i = 0; i < unoptimized_iupac_patterns.size(); i++) {
       IUPACPattern* pattern = unoptimized_iupac_patterns[i];
@@ -339,10 +348,10 @@ void Peng::process(PengParameters& params, std::vector<IUPACPattern*>& best_iupa
       std::cout << IUPACPattern::toString(pattern->get_pattern(), pattern_length) << " -> " << pattern->get_pattern_string() << std::endl;
     }
 
-    std::cout << "max_k: " << this->max_k << std::endl;
+    print_status("Optimizing expectation-maximization / merging patterns");
     for(int background = 0; background <= this->max_k; background++) {
       float* pattern_bg_probs = base_pattern->getBackgroundProb(background);
-      std::cout << "k: " << background << std::endl;
+      std::cout << std::endl << "background order: " << background << std::endl;
       std::vector<IUPACPattern*> optimized_patterns;
       if(params.use_em) {
         em_optimize_pwms(unoptimized_iupac_patterns, base_pattern,
